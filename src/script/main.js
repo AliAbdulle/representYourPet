cfe97da4c266a1257599f3905aa46a803e339a15
AliAbdulle <alidhobey@gmail.com>
2019-03-04 19:36:15 -0600

    created pets repo and added github

---
+++ src/script/main.js
@@ -0,0 +1,13 @@
+// Create an object that represents your pet.
+const pets = {
+    // Name property with a string value.
+    name: "goat",
+    // Species property with a string value.
+    spacies: "Kenya",
+    // Nicknames property with an array value. Array contains strings.
+    niknames: ["Arbic-goat", "Kenya-goat"],
+    // Age property with an integer value
+    age: 3
+}
+console.log(pets.niknames)
+
